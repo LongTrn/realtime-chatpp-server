a5f93f4827736f966cff29db21622adba8a555da
Long <longjax1401@gmail.com>
2021-05-10 14:27:51 +0700

    add controllers

---
+++ controllers/auths.controller.js
@@ -0,0 +1,312 @@
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const DatauriParser = require('datauri/parser');
+
+// const { User } = require('../models')
+const { cloudinary } = require('../middlewares/utils/cloudinary');
+const User = require('../models/user.model')
+const { optionRefreshToken, optionAccessToken } = require('../config/index.config')
+const handleErrors = (err) => {
+    // console.log(err.message, err.code);
+    let errors = { email: '', password: '' };
+  
+    // duplicate email error
+    if (err.code === 11000) {
+      errors.email = 'that email is already registered';
+      return errors;
+    }
+  
+    // validation errors
+    if (err.message.includes('user validation failed')) {
+      // console.log(err);
+      Object.values(err.errors).forEach(({ properties }) => {
+        // console.log(val);
+        // console.log(properties);
+        errors[properties.path] = properties.message;
+      });
+    }
+  
+    return errors;
+}
+
+const generateAccessToken = (user) => jwt.sign({user}, process.env.ACCESS_TOKEN_SECRET, optionAccessToken)
+const generateRefreshToken = (user) => jwt.sign({user}, process.env.REFRESH_TOKEN_SECRET)
+
+let refreshTokens = []
+const maxAge = 60
+// const maxAge = 60 * 60 * 24 * 30
+
+module.exports.index = (req, res) => {
+    try {
+        if (req.method !== 'GET') {
+            throw new Error('Invalid required method');
+        }
+        else res.redirect('/auths/login')
+    } catch (error) {
+        res.status(400).json({
+            code: 400,
+            message: error.message
+        })
+    }
+}
+
+module.exports.loginForm = (req, res) => {
+    try {
+        if (req.method !== 'GET') {
+            throw new Error('Invalid required method');
+        }
+        else res.json({layout: 'LoginFrom'})
+    } catch (error) {
+        res.status(400).json({
+            code: 400,
+            message: error.message
+        })
+    }
+}
+
+module.exports.login = async (req, res) => {
+    try {
+        if (req.method !== 'POST') {
+            throw new Error('Invalid required method');
+        }
+        else {
+            let { username, password } = req.body
+            if (![ username, password ].every(Boolean)) {
+                throw new Error('Invalid arguments required');
+            }
+            else {
+            await User.find({'locals.username': username})
+                .then(async response => {
+                    if (response.length > 0) {
+                        const user = response[0];
+                        const validPassword = await bcrypt.compare(password, user.locals.password)
+                        if (validPassword) {
+                            const accessToken = generateAccessToken(user)
+                            const refreshToken = generateRefreshToken(user)
+                            refreshTokens.push(refreshToken)
+                            res.set('Authorization', `Bearer ${accessToken}`);
+                            res.cookie('accessToken', accessToken, { 
+                                // secure: true, 
+                                httpOnly: true, 
+                                maxAge: optionAccessToken.expiresIn * 1000
+                            });
+                            res.cookie('refreshToken', refreshToken, { 
+                                // secure: true, 
+                                httpOnly: true, 
+                                maxAge: optionRefreshToken.expiresIn * 1000
+                            });
+                            return res.json({
+                                code: 200,
+                                message: 'Login successfully',
+                                accessToken, 
+                                refreshToken
+                            })
+                        }
+                        return res.json({
+                            code: 401,
+                            error: 'Login with incorrect password'
+                        })
+                        
+                    }
+                    else throw new Error('Username is not exist');
+                })
+            }
+        }
+    } 
+    catch (error) {
+        res.json({
+            code: 400,
+            message: 'Error occurs',
+            error: error.message
+        })
+    }
+}
+
+module.exports.registerForm = (req, res) => {
+    try {
+        if (req.method !== 'GET') {
+            throw new Error('Invalid required method');
+        }
+        else res.json({layout: 'RegisterForm'})
+    } 
+    catch (error) {
+        res.status(400).json({
+            code: 400,
+            message: 'Error occurs',
+            error: error.message
+        })
+    }
+}
+
+module.exports.register = async (req, res) => {
+    try {
+        if (req.method !== 'POST') {
+            throw new Error('Invalid required method');
+        }
+        else {
+            console.log(`upload file`)
+            // console.log(req.file)
+            
+            let { username, password, email, name, phone } = req.body
+            if (![ username, password, email, name, phone ].every(Boolean)) {
+                throw new Error('Invalid arguments required');
+            }
+            else {
+                await User.find({'locals.username': username, 'locals.email': email })
+                    .then(async (user) => {
+                        if (user.length > 0) {
+                            return res.json({
+                                code: 303,
+                                message: "There is an existing user",
+                            })
+                        }
+                        else {
+                            const parser = new DatauriParser();
+                            const changeToBase64 = req => {
+                                let ext = req.file.originalname.split(".")
+                                ext = `.${ext.pop()}`
+                                // console.log(ext)
+                                return parser.format(ext.toString(), req.file.buffer)
+                            } 
+
+                            if(req.file) {
+
+                                const base64 = changeToBase64(req).content;
+                                // console.log(req.file)
+                                // console.log(base64)
+                                const response = await cloudinary.uploader.upload(base64)
+                                let newUser = {
+                                    locals: {
+                                        username,
+                                        password,
+                                        name,
+                                        email,
+                                        phone,
+                                        image: response.url
+                                    },
+                                    
+                                }
+                                await User.create(newUser)
+                                    .then(result => {
+                                        if (result) {
+                                            return res.json({ 
+                                                code : 200, 
+                                                message : "Creating new user is successful",
+                                                data : newUser
+                                            })
+                                        }
+                                        else throw new Error("Creating new user is failed")
+                                    })
+                            }
+                        }
+                    })
+                    .catch(err => res.json({ 
+                        code: 404, 
+                        message: "There is an existing user",
+                    }))
+                }
+        }
+    }
+    catch (error) { 
+        res.status(400).json({
+            code: 400,
+            message: 'Error occurs',
+            error: error.message
+        })
+    }
+}
+
+module.exports.logout = (req, res) => {
+    try { 
+        if (req.method !== 'POST') {
+            throw new Error('Invalid required method');
+        }  
+        else {
+            let username = req.session.username;
+            users.map(user => user.username !== username)
+            res.json({ 
+                code: 0,
+                message: `${username} has been logged out`,
+            })
+        }     
+    }
+    catch (error) { 
+        res.status(400).json({
+            code: 400,
+            message: 'Error occurs',
+            error: error.message
+        })
+    }
+}
+
+// JWT
+module.exports.refreshToken = (req, res) => {
+    try { 
+        if (req.method !== 'POST') {
+            throw new Error('Invalid required method');
+        }  
+        else {
+
+            const signedCookies = req.signedCookies; // get signed cookies
+            console.log('signed-cookies:', signedCookies);  
+            const cookies = req.cookies; // get not signed cookies
+            console.log('not-signed-cookies:', cookies);
+            // or access directly to one cookie by its name :
+            const myTestCookie = req.signedCookies.test;
+            console.log('our test signed cookie:', myTestCookie);
+
+            const refreshToken = req.cookies.refreshToken;
+            if (refreshToken == null) return res.sendStatus(401);
+            if (!refreshTokens.includes(refreshToken)) return res.sendStatus(403)
+            jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
+                if (err) return res.sendStatus(403)
+                const accessToken = generateAccessToken(user)
+                res.set('Authorization', `Bearer ${accessToken}`);
+                res.cookie('accessToken', accessToken, { 
+                    // secure: true, 
+                    httpOnly: true, 
+                    maxAge: optionAccessToken.expiresIn * 1000
+                });
+                res.json({
+                    code: 201,
+                    message: "Refreshing token Successfully",
+                    accessToken
+                })
+            })
+        }
+    }
+    catch (error) { 
+        res.status(400).json({
+            code: 400,
+            message: 'Error occurs',
+            error: error.message
+        })
+    }
+}
+
+module.exports.deleteToken = (req, res) => {
+    try { 
+        if (req.method !== 'DELETE') {
+            throw new Error('Invalid required method');
+        }  
+        else {
+            refreshTokens = refreshTokens.filter(token => token !== req.cookies.refreshToken)
+            const hasCookies = req.cookies && req.cookies.refreshToken
+            if (hasCookies) {
+                res.clearCookie('accessToken');
+                res.clearCookie('refreshToken');
+            }
+            res.json({
+                code: 204,
+                message: "Deleting Token Successfully",
+            })
+        }
+    }
+    catch (error) { 
+        res.status(400).json({
+            code: 400,
+            message: 'Error occurs',
+            error: error.message
+        })
+    }
+}
---
+++ controllers/users.controller.js
@@ -0,0 +1,317 @@
+const bcrypt = require('bcrypt');
+const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+
+const {Message, User} = require('../models/index.model')
+const devId = '606836ab9158801258ac3498'
+const adminId = '60688ba34507a14e9caf541a'
+const testerID = '60688ae72b376c4da8dd0cce'
+// 606e81428f9ef04454f8efaa
+module.exports.user_index = (req, res) => {
+    try {
+        console.log(User)    
+        User.find({})
+            .then((listUsers) => {
+                if (listUsers.length > 0) {
+                    res.json({
+                        code: 200,
+                        message: "Getting users successfully",
+                        data: listUsers
+                    })
+                }
+                else {
+                    res.json({
+                        code: 404,
+                        message: "Getting not found users"
+                    })
+                }
+            })
+            .catch((err) => {throw new Error(err)})
+    } catch (error) {
+        if (error) {
+            res.json({
+                code: 400,
+                message: 'Error occurs', 
+                error: error.message,
+                title: "Getting users failed" 
+            })
+        }         
+    }
+}
+
+module.exports.user_create = (req, res) => {
+    try {
+        let request = req.body,
+            salt = bcrypt.genSalt(10),
+            newPassword =  bcrypt.hash(request.password, salt),
+            newProfile = {
+                locals: {
+                    username: request.username,
+                    password: newPassword,
+                    name: request.name,
+                    email: request.email,
+                    phone: request.phone,
+                }
+            }
+        User.create(newProfile)
+            .then((newUser) => {
+                if (newUser) {
+                    res.json({
+                        code: 201,
+                        message: 'Creating user successfully'
+                    })
+                }
+                else {
+                    res.json({
+                        code: 202,
+                        message: 'Creating user failed'
+                    })
+                }
+            })
+            .catch(err => {throw err})
+    } catch (error) {
+        res.json({
+            code: 400,
+            message: 'Error occurs', 
+            error: error.message
+        })
+    }
+}
+
+module.exports.user_profile = (req, res) => {
+    try {
+        User.findById(req.params.id)
+            .then((user) => {
+                if (user) {
+                    res.json({
+                        code: 200,
+                        message: "Getting profile successfully",
+                        data: user
+                    })
+                }
+                else {
+                    res.json({
+                        code: 404,
+                        message: "Getting not found profile"
+                    })
+                }
+            })
+            .catch((err) => {
+                throw err})
+    } catch (error) {
+        if (error) {
+            res.json({
+                code: 400,
+                message: error.message
+            })
+        }        
+    }
+}
+
+module.exports.user_update = (req, res) => {
+    try {
+        let _id = req.params.id
+            changes = req.body,
+            salt = bcrypt.genSalt(10),
+            newPassword =  bcrypt.hash(changes.password, salt),
+            newProfile = {
+                locals: {
+                    username: changes.username,
+                    password: newPassword,
+                    name: changes.name,
+                    email: changes.email,
+                    phone: changes.phone,
+                },
+                updated: new Date()
+            }
+        
+        let {locals: { username, email}} = newProfile,
+            newUser = new User(newProfile)
+        newUser.validate({'locals.username': username, 'locals.email': email}, ['locals.username', 'locals.email'])
+            .catch(err => {throw err});
+        User.findById(_id)
+            .then(async (user) => {
+                if (user) {
+                    User.updateOne({_id}, newProfile)
+                        .then(response => {
+                            if (response) {
+                                res.json({
+                                    code: 200,
+                                    message: 'Updating user successfully'
+                                })
+                            }
+                            else {
+                                res.json({
+                                    code: 202,
+                                    message: 'Updating user failed'
+                                })
+                            }
+                        })
+                        .catch(err => {throw err;});
+                }
+                else {
+                    User.create(newProfile)
+                        .then(response => {
+                            if(response) {
+                                res.json({
+                                    code: 201,
+                                    message: 'Creating user successfully when updating'
+                                })
+                            }
+                            else {
+                                res.json({
+                                    code: 202,
+                                    message: 'Creating user failed when updating'
+                                })
+                            }
+                        })
+                        .catch(err => {throw err;});
+                }
+            })
+            .catch((err) => {throw new Error(err)})
+    } catch (error) {
+        if (error) {
+            res.json({
+                code: 400,
+                message: 'Error occurs', 
+                error: err.message
+            })
+        }        
+    }
+}
+
+module.exports.user_delete = (req, res) => {
+    try {
+        let _id = req.params.id
+        User.findByIdAndRemove(_id)
+                .then(user => {
+                    if (user) {
+                        res.json({
+                            code: 200,
+                            message: 'Deleting user successfully'
+                        })
+                    }
+                    else {
+                        res.json({
+                            code: 404,
+                            message: 'Deleting not found user'
+                        })
+                    }
+                })
+                .catch(err => {throw err})
+        
+    } catch (error) {
+        res.json({
+            code: 400,
+            message: 'Error occurs', 
+            error: error.message
+        })
+    }
+}
+
+module.exports.user_token = (req, res) => {
+    let accessToken = req.params.token
+    if (accessToken == null) return res.sendStatus(401)
+    jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET, (error, response) => {
+        if (error) {
+            return res.json({
+                code: 403,
+                message: 'Error occurs', 
+                error: error.message
+            });
+        }
+        if (response) {
+            let user = response.user
+            console.log(response)
+            return res.json({
+                code: 200,
+                message: "Getting user with valid token successfully",
+                user: {
+                    _id: user._id,
+                    name: user.locals.name,
+                    image: user.locals.image,
+                }
+            })
+        }
+        return res.json({
+            code: 403, 
+            message: "Getting user failed with invalid token"
+        })
+    })
+}
+
+module.exports.user_conversation = (req, res) => {
+    try {
+        let { senderId } = req.body,
+            userId = senderId
+            limit= 50,
+            skip = 0
+        // senderId = adminId
+
+        Message.find({
+            $or:[
+                {senderId: userId, receiverId: {$ne: userId}},
+                {senderId: {$ne: userId}, receiverId: userId},
+            ]})
+            .skip(skip)
+            .limit(limit)
+            .distinct('senderId') // check distinct sender before receiver to get self conversation
+            .distinct('receiverId')
+            .then((response) => {
+                console.log(response)
+                if (response.length > 0) {
+                    // return res.json({
+                    //     code: 200,
+                    //     message: "Successfully",
+                    //     data: response
+                    // })
+                    // return Message.find({
+                    //             senderId: userId, 
+                    //             receiverId: {$in: response, $ne: userId},
+                    // })
+                    
+                    return User.find({_id: {$in: response}})
+                }
+                throw new Error("Can't find any user in conversation with");
+            })
+            // .then(response => {
+            //     res.json({resposnse})
+            // })
+            .then(response => {
+                console.log(response)
+                if (response.length > 0) {
+                    return res.json({
+                        code: 200,
+                        message: "Getting conversation successfully",
+                        data: response
+                    })
+                }
+                throw new Error("None user ever chat with")
+            })
+            .catch((error) => {throw error})
+
+        
+    } catch (error) {
+        res.json({
+            messages: error.messages
+        })
+    }
+}
+
+module.exports.user_search = (req, res) => {
+    let { search, userId } = req.body;
+    console.log(`search`)
+    console.log(search)
+    User.find({"locals.name": {$regex: `.*${search}.*`}})
+    // User.find({$text: search})
+        .then(response => {
+            response = response.filter(user => user._id != userId)
+            console.log(`Search Response list user`)
+            console.log(response)
+            res.json({
+                users: response
+            })
+        })
+        .catch(error => res.json({error: error.message}))
+        
+}
